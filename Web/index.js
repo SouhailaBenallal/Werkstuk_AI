91b78ab3c2e84134eb98fa47061a3b20375e4a78
souhaila <souhaila@adshot.io>
2021-05-16 13:03:15 +0200

    Index

---
+++ Web/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log('Hello TensorFlow');
+
+async function getData() {
+   const carsDataResponse = await fetch('/Python/.keras_model/model.json');  
+   const carsData = await carsDataResponse.json();  
+   console.log(carsData,'Model');
+ }
+getData();
